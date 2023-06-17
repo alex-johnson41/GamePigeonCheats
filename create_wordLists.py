e0503dd94667ab1c2d9a4449b4f51046f565979b
alex-johnson41 <alex@heathjohnson.org>
2023-06-17 14:50:23 -0500

    Remove unneeded files

--- create_wordLists.py
+++
@@ -1,8 +0,0 @@
-
-with open('word_list.txt', 'r') as f1, open('anagrams_wordList.txt', 'w') as f2:
-    for line in f1:
-        if len(line) > 3 and len(line) < 8: f2.write(line)
-
-file = open('word_list.txt')
-
-
