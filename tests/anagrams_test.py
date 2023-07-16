309c34498bf423405e65c000248e668cb2f226ce
alex-johnson41 <alex@heathjohnson.org>
2023-07-16 14:55:35 -0500

    Change function names to match project naming convention

--- tests/anagrams_test.py
+++ tests/anagrams_test.py
@@ -3,17 +3,17 @@ from solvers.anagrams_solver import Anagrams_Solver
 
 class test_anagrams(unittest.TestCase):
 
-    def test_six_letters(self):
+    def testSixletters(self):
         solver = Anagrams_Solver(['t', 'e', 'l', 'l', 'h', 'y'])
         x = solver.solve()
         self.assertEqual(x, ['theyll', 'telly', 'lythe', 'lethy', 'helly', 'ethyl', 'yeth', 'yelt', 'yell', 'they', 'tell', 'hyte', 'hyle', 'htel', 'hell', 'yet', 'yeh', 'tye', 'thy', 'the', 'tel', 'lye', 'ley', 'let', 'hye', 'hey', 'het', 'hel', 'eyl', 'ety', 'eth', 'elt', 'ell'])
 
-    def test_five_letters(self):
+    def testFiveLetters(self):
         solver = Anagrams_Solver(['s', 'a', 'g', 't', 'e'])
         x = solver.solve()
         self.assertEqual(x, ['stage', 'getas', 'geast', 'gates', 'tegs', 'tega', 'teas', 'tags', 'stge', 'steg', 'stag', 'seta', 'seat', 'sate', 'sage', 'gets', 'geta', 'gest', 'geat', 'gats', 'gate', 'gast', 'gaet', 'gaes', 'etas', 'eats', 'east', 'ates', 'agst', 'aget', 'ages', 'teg', 'tea', 'tas', 'tag', 'tae', 'stg', 'sta', 'set', 'seg', 'sea', 'sat', 'sag', 'sae', 'gte', 'get', 'ges', 'gat', 'gas', 'gae', 'eta', 'est', 'eat', 'ate', 'ast', 'asg', 'ase', 'agt', 'age', 'aet', 'aes'])
 
-    def test_input_order(self):
+    def testInputOrder(self):
         solver1 = Anagrams_Solver(['t', 'e', 'l', 'l', 'h', 'y'])
         solver2 = Anagrams_Solver(['t', 'h', 'e', 'y', 'l', 'l'])
         x = solver1.solve()
