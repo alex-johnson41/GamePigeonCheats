30c8143f36fad231ef901de4ff3268df587870a4
alex-johnson41 <alex@heathjohnson.org>
2023-07-16 14:58:28 -0500

    Remove long lines

--- tests/anagrams_test.py
+++ tests/anagrams_test.py
@@ -6,12 +6,23 @@ class test_anagrams(unittest.TestCase):
     def testSixletters(self):
         solver = Anagrams_Solver(['t', 'e', 'l', 'l', 'h', 'y'])
         x = solver.solve()
-        self.assertEqual(x, ['theyll', 'telly', 'lythe', 'lethy', 'helly', 'ethyl', 'yeth', 'yelt', 'yell', 'they', 'tell', 'hyte', 'hyle', 'htel', 'hell', 'yet', 'yeh', 'tye', 'thy', 'the', 'tel', 'lye', 'ley', 'let', 'hye', 'hey', 'het', 'hel', 'eyl', 'ety', 'eth', 'elt', 'ell'])
+        self.assertEqual(x, ['theyll', 'telly', 'lythe', 'lethy', 'helly', 'ethyl', 
+                             'yeth', 'yelt', 'yell', 'they', 'tell', 'hyte', 'hyle', 
+                             'htel', 'hell', 'yet', 'yeh', 'tye', 'thy', 'the', 'tel', 
+                             'lye', 'ley', 'let', 'hye', 'hey', 'het', 'hel', 'eyl', 
+                             'ety', 'eth', 'elt', 'ell'])
 
     def testFiveLetters(self):
         solver = Anagrams_Solver(['s', 'a', 'g', 't', 'e'])
         x = solver.solve()
-        self.assertEqual(x, ['stage', 'getas', 'geast', 'gates', 'tegs', 'tega', 'teas', 'tags', 'stge', 'steg', 'stag', 'seta', 'seat', 'sate', 'sage', 'gets', 'geta', 'gest', 'geat', 'gats', 'gate', 'gast', 'gaet', 'gaes', 'etas', 'eats', 'east', 'ates', 'agst', 'aget', 'ages', 'teg', 'tea', 'tas', 'tag', 'tae', 'stg', 'sta', 'set', 'seg', 'sea', 'sat', 'sag', 'sae', 'gte', 'get', 'ges', 'gat', 'gas', 'gae', 'eta', 'est', 'eat', 'ate', 'ast', 'asg', 'ase', 'agt', 'age', 'aet', 'aes'])
+        self.assertEqual(x, ['stage', 'getas', 'geast', 'gates', 'tegs', 'tega', 'teas', 
+                             'tags', 'stge', 'steg', 'stag', 'seta', 'seat', 'sate', 'sage', 
+                             'gets', 'geta', 'gest', 'geat', 'gats', 'gate', 'gast', 'gaet', 
+                             'gaes', 'etas', 'eats', 'east', 'ates', 'agst', 'aget', 'ages', 
+                             'teg', 'tea', 'tas', 'tag', 'tae', 'stg', 'sta', 'set', 'seg', 
+                             'sea', 'sat', 'sag', 'sae', 'gte', 'get', 'ges', 'gat', 'gas', 
+                             'gae', 'eta', 'est', 'eat', 'ate', 'ast', 'asg', 'ase', 'agt', 
+                             'age', 'aet', 'aes'])
 
     def testInputOrder(self):
         solver1 = Anagrams_Solver(['t', 'e', 'l', 'l', 'h', 'y'])
